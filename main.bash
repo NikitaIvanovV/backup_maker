95796a7768a3cb2d7842c4d64dfda911575b4494
ViChyavIn <nikita.vyach.ivanov@gmail.com>
2021-08-22 02:38:54 +0500

    Raise error if the same name is added multiple times

--- main.bash
+++ main.bash
@@ -18,12 +18,19 @@ if ! backup_list=$(get_backup_list "$backup_list_file"); then
     exit_script 1
 fi
 
+all_names=()
+
 (IFS=$'\n'; for item in $backup_list; do
     name_path=$(get_backup_name_path $item)
     _exit_code=$?
     if [ $_exit_code -eq 0 ]; then
         name=$(echo "$name_path" | head -1)
         path=$(echo "$name_path" | tail -1)
+        if [[ "${all_names[*]}" =~ "$name" ]]; then
+            echo "Error: name \"$name\" defined multiple times"
+            exit_script 1
+        fi
+        all_names+="$name"
         if output=$(backup_dir "$name" "$path" "$backup_dir"); then
             echo "Backup for \"$name\" ($path) was created successfully"
         else
